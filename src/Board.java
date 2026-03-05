ec2728b901d1580640d2f9d11c1c16d43688a559
herderboys <alexander.herder@gmail.com>
2026-03-05 17:04:48 +0100

    Implement checkDirection(),  checkWinner() and getBoardState() methods

--- src/Board.java
+++ src/Board.java
@@ -5,21 +5,54 @@ public class Board {
     a long is 64 bits, and 64 x 8 = 512 which 450 fits into.
      */
     private final long[] board = new long[8];
-    private final int BOARD_HEIGHT = 15;
-    private final int PIECE_LENGTH = 2;
+    private final int size ; // how many pieces to a side
+    private final int pieceLength;
+
+    public Board(int size, int pieceLength) {
+        this.size = size;
+        this.pieceLength = pieceLength;
+    }
+
+    public int[] getLegalMoves() {
+        int emptyPlaces = 0;
+
+        // loop once to get number of available moves
+        for (int row = 0; row < size; row++) {
+            for (int col = 0; col < size; col++) {
+                if (getPiece(row, col) == 0) {
+                    emptyPlaces++;
+                }
+            }
+        }
+
+        // create array with place for all available moves
+        int[] legalMoves = new int[emptyPlaces];
+
+        // add index for available moves in array
+        int legalMovesIndex = 0;
+        for (int row = 0; row < size; row++) {
+            for (int col = 0; col < size; col++) {
+                if (getPiece(row, col) == 0) {
+                    legalMoves[legalMovesIndex] = getIndex(row, col);
+                    legalMovesIndex++;
+                }
+            }
+        }
+        return legalMoves;
+    }
 
     /*
     choosing to set pieces using bit manipulation to optimize performance
     ( and also because I thought it was really fun in CPROG :) )
      */
-    public void setPiece(int row, int col, int player) {
+    public void setPiece(int row, int col, long player) {
         if (player < 0 || player > 2) {
-            throw new IllegalArgumentException("Player must be either 1, 2 or 0 to clear piece.");
+            throw new IllegalArgumentException("Player must be either 1 or 2. It can also be set to 0 to clear piece.");
         }
 
-        int arrayIndex = getIndex(row, col);
+        int arrayIndex = getInternalIndex(row, col);
         // where exactly is the leading bit of the piece?
-        int bitOffset = (row * (BOARD_HEIGHT * PIECE_LENGTH) + col * PIECE_LENGTH) % 64;
+        int bitOffset = (row * (size * pieceLength) + col * pieceLength) % 64;
 
         // make a mask clearing bits we want to overwrite
         long clearMask = ~(3L << bitOffset);
@@ -28,14 +61,36 @@ public class Board {
         board[arrayIndex] &= clearMask;
 
         // prepare mask for which player to apply to piece
-        long pieceMask = ((long)player) << bitOffset;
+        long pieceMask = player << bitOffset;
 
         // place player mask into correct piece
         board[arrayIndex] |= pieceMask;
     }
 
-    public int getIndex(int row, int col) {
-        // divide by 64 to get the absolute first bit of the long, since a long is 64 bits
-        return (row * (BOARD_HEIGHT * PIECE_LENGTH) + col * PIECE_LENGTH) / 64;
+    public long getPiece(int row, int col) {
+
+        int arrayIndex = getInternalIndex(row, col);
+        int bitOffset = (row * (size * pieceLength) + col * pieceLength) % 64;
+
+        // shift bits at offset to the absolute right of long
+        long ret = board[arrayIndex] >> bitOffset;
+
+        // wipe remaining bits that are not part of first 2 (3L == 0b11)
+        ret &= 3L;
+
+        return ret;
     }
+
+    public int getIndex(int row, int col) {
+        return row * size + col;
+    }
+
+    public int getSize() {
+        return size;
+    }
+    private int getInternalIndex(int row, int col) {
+        // divide by 64 to get the absolute first bit of the long, since a long is 64 bits
+        return (row * (size * pieceLength) + col * pieceLength) / 64;
+    }
+
 }
--- src/Brain.java
+++ src/Brain.java
@@ -1,2 +1,71 @@
 public class Brain {
+
+    private final Board board;
+
+    public Brain(Board board) {
+        this.board = board;
+    }
+
+    public int minimax(int depth, int alpha, int beta, boolean isMaximizingPlayer) {
+
+    }
+
+    public long checkWinner() {
+        for (int row = 0; row < 15; row++) {
+            for (int col = 0; col < 15; col++) {
+                long currentPiece = board.getPiece(row, col);
+
+                // if square is empty, no way to win for anyone, move on
+                if (currentPiece == 0) {
+                    continue;
+                }
+
+                // checking all valid directions
+                if (checkDirection(row, col, 0, 1, currentPiece, 5) ||
+                        checkDirection(row, col, 1, 0, currentPiece, 5) ||
+                        checkDirection(row, col, 1, 1, currentPiece, 5) ||
+                        checkDirection(row, col, -1, 1, currentPiece, 5)) {
+                    return currentPiece;
+                }
+            }
+        }
+        return 0;
+    }
+
+    /*
+    helper method to check 5 in a row for a certain direction.
+    dRow is direction of row, dCol is direction of column
+    0, 1 to move right (horizontal)
+    1, 0 to move down (vertical)
+    1, 1 to move down-right
+    -1, 1 to move up-right
+
+    returns true if there are n in a row for player, where n = length
+     */
+
+    public boolean checkDirection(int startRow, int startCol, int dRow, int dCol, long player, int length) {
+
+        // -1 because starting piece counts as first piece
+        int endRow = startRow + ((length - 1) * dRow);
+        int endCol = startCol + ((length - 1) * dCol);
+
+        if (endRow > board.getSize() - 1 || endRow < 0 || endCol < 0 || endCol > board.getSize() - 1) {
+            return false;
+        }
+
+        int count = 0;
+        int row = startRow;
+        int col = startCol;
+
+        while (count < length) {
+            if (board.getPiece(row, col) == player) {
+                count++;
+            } else {
+                return false;
+            }
+            row += dRow;
+            col += dCol;
+        }
+        return count == length;
+    }
 }
